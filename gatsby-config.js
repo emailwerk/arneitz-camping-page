21738817ae1c1edb747989fca9b3e6752ded563d
petute <daniel.petutschnigg@gmail.com>
2021-09-17 22:42:39 +0200

    chore: add temporary favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,7 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: 'standalone',
-        icon: '' // This path is relative to the root of the site.
+        icon: 'src/images/ant.png' // This path is relative to the root of the site.
       }
     }
   ]

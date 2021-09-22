c6a9f01860df39102f13302f9a012a4fd9275f81
petute <daniel.petutschnigg@gmail.com>
2021-09-22 02:40:38 +0200

    feat: add campingmap lightbox

--- src/components/molecules/CampingMap/index.tsx
+++ src/components/molecules/CampingMap/index.tsx
@@ -1,5 +1,30 @@
+import LightBox from '../../LightBox'
+import {fields} from '@snek-at/jaen-pages'
+
 const CampingMap = () => {
-  return null
+  return (
+    <LightBox
+      previewImage={
+        <fields.ImageField
+          fieldName="campingmap-lightbox"
+          initValue={{
+            src: 'https://i.ibb.co/J2jzkBx/placeholder.jpg',
+            alt: 'lightboximage'
+          }}
+          className="campingmapPreview"
+        />
+      }>
+      <fields.ImageField
+        fieldName="campingmap-lightbox"
+        initValue={{
+          src: 'https://i.ibb.co/J2jzkBx/placeholder.jpg',
+          alt: 'lightboximage'
+        }}
+        className="campingMap"
+        style={{width: '100%'}}
+      />
+    </LightBox>
+  )
 }
 
 export default CampingMap
